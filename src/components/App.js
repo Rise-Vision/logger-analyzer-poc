d8d3d28a8a083249dd97263dc5bb69654677be59
santiagonoguez <santiago@nul-unu.com>
2019-01-09 14:22:43 -0500

    use all width

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from "react";
+
 import "../stylesheets/main.scss";
 
-// app component
-export default class App extends React.Component {
-  // render
-  render() {
+export default class App extends React.Component
+{
+  render()
+  {
     return (
-      <div className="container">
-        {this.props.children}
+      <div className="container-flow">
+        { this.props.children }
       </div>
-    );
+    )
   }
 }
